b612f298c2dc36967a6b2edc18bdb9548a5dd5ad
Dariusz Okoński <darek200180@gmail.com>
2024-04-17 06:49:09 +0200

    add filtering

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,21 @@ const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
 
+  const filterItems = (category) => {
+    if (category === 'all') {
+      return setMenuItems(menu);
+    }
+
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <main>
       <section className='menu'>
         <Title text='our menu' />
-        <Categories categories={categories} />
-        <Menu items={menu} />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
